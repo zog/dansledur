08c47e6c1c485b0195c6496834cb740524bac9b2
Johan Van Ryseghem <johan.van.ryseghem@wopata.com>
2011-09-28 01:19:55 +0200

    Use US S3 bucket

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class Medium < ActiveRecord::Base
   has_attached_file :image, 
       :storage => :s3,
-      :bucket => 'dansledur',
+      :bucket => 'dans-le-dur',
       :s3_credentials => {
         :access_key_id => ENV['S3_KEY'],
         :secret_access_key => ENV['S3_SECRET']
