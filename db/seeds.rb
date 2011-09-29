5d00f09a3f9403495eee99213d83641911e942c3
Johan Van Ryseghem <johan.van.ryseghem@wopata.com>
2011-09-29 22:55:54 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create email: "johan@about-blank.fr", password: "boulou"
