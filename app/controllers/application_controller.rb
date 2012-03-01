a8446a774717700e142a9a559fbcc3762170ffdb
Johan Van Ryseghem <johan.van.ryseghem@wopata.com>
2012-03-01 14:16:35 +0100

    Use 404 page everywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  unless  ActionController::Base.consider_all_requests_local
+    rescue_from Exception, :with => :not_found
+  end
+
+
   layout 'application'
   
   def not_found
