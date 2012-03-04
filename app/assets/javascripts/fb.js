cb645913715b963a14036eb3b3a1d6f835009b35
Johan Van Ryseghem <johan.van.ryseghem@wopata.com>
2012-03-04 13:53:42 +0100

    Cloudfront

---
+++ app/assets/javascripts/fb.js
@@ -0,0 +1,4 @@
+function fbs_click() { 
+  u=location.href; t=document.title; 
+  window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),' sharer', 'toolbar=0, status=0, width=626, height=436'); return false; 
+} 
