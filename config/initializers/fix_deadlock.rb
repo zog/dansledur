a47ce931f5efbd933a7e36d85f9db5f780abf2c0
Johan Van Ryseghem <johan.van.ryseghem@wopata.com>
2012-03-04 21:02:30 +0100

    Fix deadlocks

---
+++ config/initializers/fix_deadlock.rb
@@ -0,0 +1,15 @@
+require "thread"
+class Mutex
+  def lock_with_hack
+    lock_without_hack
+  rescue ThreadError => e
+    if e.message != "deadlock; recursive locking"
+      raise
+    else
+      unlock
+      lock_without_hack
+    end
+  end
+  alias_method :lock_without_hack, :lock
+  alias_method :lock, :lock_with_hack
+end
