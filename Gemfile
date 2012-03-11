65f3c5745d76b47660a4d49026883d195c1989f7
Johan Van Ryseghem <johan.van.ryseghem@wopata.com>
2012-03-11 14:14:18 +0100

    Use DJ

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ gem 'will_paginate', '~> 3.0'
 #gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
 gem 'bitly'
 gem 'rack-google-analytics', :require => 'rack/google-analytics'
-#gem 'delayed_job_active_record'
+gem 'delayed_job_active_record'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/medium.rb
+++ app/models/medium.rb
@@ -10,7 +10,7 @@ class Medium < ActiveRecord::Base
     has_attached_file :image,
         :storage => Rails.env.production? ? :s3 : :filesystem,
         :bucket => 'dans-le-dur',
-        #:s3_alias_url => 'cdn.dansledur.com',
+        :s3_alias_url => 'cdn.dansledur.com',
         :s3_credentials => {
           :access_key_id => ENV['S3_KEY'],
           :secret_access_key => ENV['S3_SECRET']
